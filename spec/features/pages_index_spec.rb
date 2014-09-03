5cc25b85d6e9bbd725d3aa3c2fad6224f2ea2cf5
supermomonga <hi@supermomonga.com>
2014-09-03 17:37:27 +0900

    pages index spec

---
+++ spec/features/pages_index_spec.rb
@@ -0,0 +1,10 @@
+# encoding: utf-8
+
+require 'spec_helper'
+
+describe 'トップページ' do
+  specify 'ランディングページを表示' do
+    visit root_path
+    expect(page).to have_css('p', text: 'Hello World!')
+  end
+end
