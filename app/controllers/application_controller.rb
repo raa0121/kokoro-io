54b687e7a2426db1cc59dc1b603a46d3a059c485
supermomonga <hi@supermomonga.com>
2014-09-16 04:28:06 +0900

    detect locale

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,11 +4,16 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   helper_method :current_user
   load_and_authorize_resource
+  before_action :detect_locale
 
   private
 
   def current_user
     @current_user ||= User.where(id: session[:user_id]).first if session
+  end
+
+  def detect_locale
+    I18n.locale = request.headers['Accept-Language'].scan(/^[a-z]{2}/).first
   end
 
 end
