e4101e7e37a22492708c09e75888c554f7f32429
supermomonga <hi@supermomonga.com>
2014-10-10 15:09:11 +0900

    fix user_name generation

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,18 +3,18 @@ class SessionsController < ApplicationController
 
   def create
     auth = request.env['omniauth.auth']
+    user_name = User.uniq_user_name auth.info.nickname
     user = User.where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.provider = auth.provider
       user.uid = auth.uid
       user.screen_name = auth.info.name
-      user.user_name = User.uniq_user_name auth.info.nickname
+      user.user_name = user_name
       user.avatar_url = auth.info.image
 
       essential_token = user.access_tokens.new
       essential_token.name = '-'
       essential_token.token = AccessToken.generate_token
       essential_token.essential = true
-
       user.save
     end
     session[:user_id] = user.id
