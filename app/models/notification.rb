c0618aec557ecbfbb2af8f7d7cb68891b8e73ee0
supermomonga <hi@supermomonga.com>
2014-10-13 22:49:33 +0900

    validation

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,3 +1,5 @@
 class Notification < ActiveRecord::Base
   belongs_to :user
+  validates :user, :title, :message, :redirect_url, :image_url, presence: true
+  validates :read, inclusion: {in: [true, false]}
 end
