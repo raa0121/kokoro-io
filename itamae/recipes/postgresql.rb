37be80c3874e10ca149634fd5c6890864fae4b11
supermomonga <hi@supermomonga.com>
2014-11-06 03:41:08 +0900

    pg settings

--- itamae/recipes/postgresql.rb
+++ itamae/recipes/postgresql.rb
@@ -5,3 +5,9 @@ package "postgresql"
 package "postgresql-contrib"
 package "libpq-dev"
 
+execute "create postgresql user '#{node["pg"]["user"]}' and database '#{node["pg"]["database"]}'" do
+  command %`echo "CREATE USER #{node["pg"]["user"]} WITH PASSWORD '#{node["pg"]["password"]}';"` | sudo -u postgres psql
+  command %`echo "CREATE DATABASE #{node["pg"]["database"]} OWNER #{node["pg"]["user"]};"` | sudo -u postgres psql
+  not_if %`sudo -u postgres psql postgres -tAc "SELECT 1 from pg_roles WHERE rolname='#{node["pg"]["user"]}';"`
+end
+
