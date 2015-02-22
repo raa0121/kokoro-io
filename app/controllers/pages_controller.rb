96ee9108521c18bdde8642f81cc02bdce5078457
supermomonga <hi@supermomonga.com>
2015-02-22 22:48:43 +0900

    chat page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,10 @@ class PagesController < ApplicationController
   skip_load_and_authorize_resource
 
   def index
+    if current_user
+      render :chat, layout: 'chat'
+    else
+    end
   end
 
 end
