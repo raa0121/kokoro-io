16cd1d8ee69834996f8528cd8b2ebc127b7b2f2f
mtwtkman <punipuniomochi@gmail.com>
2016-12-14 19:36:02 +0900

    pending

--- spec/requests/api/v1/rooms_spec.rb
+++ spec/requests/api/v1/rooms_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe API::Root::V1::Rooms, type: :request do
-  let(:rooms) { FactoryGirl.create_list(:room, 10) }
   let(:user) { FactoryGirl.create(:user) }
+  let(:rooms) { user.chattable_rooms }
   let(:headers) { {'X-Access-Token' => user.access_tokens.first.token} }
   describe 'GET /api/v1/rooms' do
     it 'returns all chattable rooms' do
@@ -13,11 +13,28 @@ RSpec.describe API::Root::V1::Rooms, type: :request do
   end
 
   describe 'GET /api/v1/rooms/:id' do
-    it 'returns a specified room by room id' do
-      room = user.chattable_rooms.first
+    let(:room) { user.chattable_rooms.first }
+    it 'returns 200 status' do
       get "/api/v1/rooms/#{room.id}", headers: headers, params: {}
       expect(response.status).to eq(200)
+    end
+
+    it 'returns an information about queried room' do
+      get "/api/v1/rooms/#{room.id}", headers: headers, params: {}
       expect(response.body).to eq(room.to_json)
+    end
+  end
+
+  describe 'POST /api/v1/rooms' do
+    it 'successes to create a new room' do
+      pending('FIXME')
+      params = {
+        room_name: 'nice_heya',
+        screen_name: 'nice_mieru_heya',
+        private: false,
+        description: 'this room is absolutely nice'
+      }
+      expect {post '/api/v1/rooms', params: params}.to change(Room, :count).by(1)
     end
   end
 end
