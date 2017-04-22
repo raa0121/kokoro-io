1e14fbded6bab56ce3b525939ad9f0cad727144f
supermomonga <hi@supermomonga.com>
2017-04-22 20:02:03 +0900

    access token in meta tag

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,7 @@ class PagesController < ApplicationController
 
   def index
     if current_user
+      @access_token = current_user.primary_access_token
       render :chat, layout: 'chat'
     else
     end
