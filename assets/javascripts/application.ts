a236f5bfe83fbd8ea3d8c7e12e73b566a472f9ea
kamichidu <c.kamunagi@gmail.com>
2017-04-22 20:50:29 +0900

    show room list by javascript

--- assets/javascripts/application.ts
+++ assets/javascripts/application.ts
@@ -4,19 +4,14 @@ import axios, { AxiosRequestConfig, AxiosPromise } from 'axios';
 import * as ActionCable from 'actioncable';
 declare function require(name: string);
 // const roomChannel= require('./channels/room.ts');
-const messagesView= require('./view/messages.vue');
-const messageInputView= require('./view/message-input.vue');
+const messagesView = require('./view/messages.vue');
+const messageInputView = require('./view/message-input.vue');
+const roomsView = require('./view/rooms.vue');
 
 // Initialize global context
 const App = {
     cable: ActionCable.createConsumer(),
 };
-
-(<any>Vue.prototype).$http = axios.create({
-    xsrfHeaderName: 'X-CSRF-Token',
-    withCredentials: true,
-    baseURL: '/api'
-});
 
 class ApiClient
 {
@@ -67,10 +62,29 @@ const data= {
 };
 
 document.addEventListener('DOMContentLoaded', () => {
+    // get access token via DOM
+    const accessToken = document.head.querySelector('meta[name="access-token"]').getAttribute('content');
+    // automatically insert some headers for all requests
+    (<any>Vue.prototype).$http = axios.create({
+        xsrfHeaderName: 'X-CSRF-Token',
+        withCredentials: true,
+        baseURL: '/api',
+        headers: {
+            'X-Access-Token': accessToken,
+        },
+    });
+
     const MessagesView = Vue.extend(messagesView);
     const MessageInputView = Vue.extend(messageInputView);
+    const RoomsView = Vue.extend(roomsView);
 
     const eventBus = new Vue();
+    new RoomsView({
+        el: '#chatapp .sidebar',
+        propsData: {
+            eventBus: eventBus,
+        },
+    });
     new MessagesView({
         el: '#chatapp .talks',
         propsData: {
---
+++ assets/javascripts/view/rooms.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="col-sm-3 col-md-2 sidebar">
+        <ul class="nav nav-sidebar">
+            <li v-for="room in rooms">
+                <a v-bind:href="roomUrl(room)"><span class="badge pull-right">1</span>{{room.screen_name}}</a>
+            </li>
+        </ul>
+        <hr>
+        <a class="btn btn-primary btn-block btn-sm" href="/rooms/new">Create room</a>
+        <a class="btn btn-default btn-block btn-sm" href="/rooms">Join room</a>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            eventBus: {
+                required: true,
+            },
+        },
+
+        data(){
+            return {
+                // memo: [{
+                //   "id":1,
+                //   "room_name":"aaaa",
+                //   "screen_name":"aaaa",
+                //   "private":false,
+                //   "created_at":"2017-04-22T11:00:02.354Z",
+                //   "updated_at":"2017-04-22T11:00:02.354Z",
+                //   "description":"eeee",
+                // }]
+                rooms: [],
+            };
+        },
+
+        mounted(){
+            const promise = this.$http.get(`/v1/rooms`);
+            promise.then(response => {
+                this.rooms = [];
+                (response.data || []).forEach(room => this.rooms.push(room));
+            });
+        },
+
+        methods: {
+            roomUrl(room){
+                return `/rooms/${room.id}`;
+            },
+        },
+    };
+</script>
