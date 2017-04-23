858550e1104b9f3a2fb3a08b09561d9706af656d
supermomonga <hi@supermomonga.com>
2017-04-24 07:12:28 +0900

    use devise's current_user

--- config/initializers/swagger.rb
+++ config/initializers/swagger.rb
@@ -4,7 +4,6 @@ GrapeSwaggerRails.options.url      = '/swagger_doc.json'
 GrapeSwaggerRails.options.doc_expansion = 'list'
 
 GrapeSwaggerRails.options.before_action do |request|
-  current_user = User.find_by(id: session[:user_id])
 
   GrapeSwaggerRails.options.app_url = request.protocol + request.host_with_port + '/api/v1'
   if Rails.env.development? && current_user && current_user.primary_access_token
