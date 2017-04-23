9348b6fc27b6d6d86233568767a51e9fced421c6
supermomonga <hi@supermomonga.com>
2017-04-23 17:23:56 +0900

    add some validates for profile

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -4,6 +4,9 @@ class Profile < ApplicationRecord
 
   belongs_to :publisher, polymorphic: true
   validates :display_name, :screen_name, presence: true
+  validates :available, inclusion: {in: [true, false]}
+  validates :screen_name, uniqueness: true
+
 
   attachment :avatar, destroy: false
   has_many :messages, as: :publisher
