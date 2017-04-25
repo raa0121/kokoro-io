00a76d3fdd8362279be53464636db586b2a3ba02
supermomonga <hi@supermomonga.com>
2017-04-25 09:17:14 +0900

    show lock icon if room is private

--- assets/javascripts/view/rooms.vue
+++ assets/javascripts/view/rooms.vue
@@ -2,7 +2,7 @@
     <div class="col-sm-3 col-md-2 sidebar">
         <ul class="nav nav-sidebar">
             <li v-bind:class="{ active: isActive(room) }" v-on:click.capture.prevent="changeRoom(room)" v-for="room in rooms">
-                <a v-bind:href="roomUrl(room)"><span class="badge pull-right">1</span>{{room.screen_name}}</a>
+                <a v-bind:href="roomUrl(room)"><span class="badge pull-right">1</span>{{room.screen_name}}<i class="fa fa-lock" v-if="room.private" /></a>
             </li>
         </ul>
         <hr>
