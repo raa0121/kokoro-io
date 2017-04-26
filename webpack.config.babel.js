237ce3b329a08f32fb934a8804ca358d87a15eff
kamichidu <c.kamunagi@gmail.com>
2017-04-27 00:31:40 +0900

    use webpack 2

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,5 +1,5 @@
+import webpack from 'webpack';
 import path from 'path';
-const webpack = require('webpack');
 
 export default {
     entry: {
@@ -11,21 +11,21 @@ export default {
         filename: '[name].js',
     },
     resolve: {
-        extensions: ['', '.webpack.js', '.web.js', '.ts', '.js', '.vue'],
+        extensions: ['*', '.webpack.js', '.web.js', '.ts', '.js', '.vue'],
         alias: {
             vue: 'vue/dist/vue.js',
         },
     },
     plugins: [
-        new webpack.ProvidePlugin({
-            jQuery: "jquery",
-            $: "jquery"
-        })
+        // new webpack.ProvidePlugin({
+        //     jQuery: "jquery",
+        //     $: "jquery"
+        // })
     ],
     module: {
         loaders: [
-            { test: /\.ts$/,  loaders: ['babel', 'ts'], exclude: /node_modules/ },
-            { test: /\.vue$/, loaders: ['vue'],         exclude: /node_modules/ },
+            { test: /\.ts$/,  loaders: ['babel-loader', 'ts-loader'], exclude: /node_modules/ },
+            { test: /\.vue$/, loaders: ['vue-loader'],                exclude: /node_modules/ },
         ],
     },
     devtool: '#source-map',
