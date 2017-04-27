468702cee5fdc6979ccb0417c305d98d33e57673
supermomonga <hi@supermomonga.com>
2017-04-28 07:59:10 +0900

    string

--- app/channels/chat_channel.rb
+++ app/channels/chat_channel.rb
@@ -20,7 +20,7 @@ class ChatChannel < ApplicationCable::Channel
       if (room = user.chattable_rooms.find_by(screen_name: screen_name))
         # Subscribe only if not subscribed yet
         # github.com/rails/rails/blob/9588a3d66d4ca6ba122d32417aa62680f441bf40/actioncable/lib/action_cable/channel/streams.rb#L83
-        stream_from room.id unless streams.find { |(b, _)| b == room.id }
+        stream_from room.id unless streams.find { |(b, _)| b == String(room.id) }
       end
     end
   end
