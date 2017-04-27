398d6362699952672a0f03528b581656a91f4af5
supermomonga <hi@supermomonga.com>
2017-04-28 08:08:59 +0900

    workaround to fix modal.js error

--- assets/javascripts/view/message-input.vue
+++ assets/javascripts/view/message-input.vue
@@ -1,5 +1,5 @@
 <template>
-    <textarea ref="sayWidget" id="say_text" placeholder="Let's talk!" v-bind:rows="inputLines" v-bind:disabled="disabled" v-model="text" v-on:keypress="maybeSay($event)"></textarea>
+    <textarea ref="sayWidget" name="say_text" id="say_text" placeholder="Let's talk!" v-bind:rows="inputLines" v-bind:disabled="disabled" v-model="text" v-on:keypress="maybeSay($event)"></textarea>
 </template>
 
 <script>
