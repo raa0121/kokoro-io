773d4a81337ddee186739f1b4335ea4318a92613
kamichidu <c.kamunagi@gmail.com>
2017-04-30 01:43:46 +0900

    simplify DOMContentLoaded handler

--- assets/javascripts/chat.ts
+++ assets/javascripts/chat.ts
@@ -38,61 +38,72 @@ class ApiClient {
     }
 }
 
+class Chat {
+    accessToken: string;
+
+    constructor(accessToken: string) {
+        this.accessToken = accessToken;
+    }
+
+    start(): void {
+        // automatically insert some headers for all requests
+        (Vue.prototype as any).$http = axios.create({
+            xsrfHeaderName: 'X-CSRF-Token',
+            withCredentials: true,
+            baseURL: '/api',
+            headers: {
+                'X-Access-Token': this.accessToken,
+            },
+        });
+        // can access global configuration
+        (Vue.prototype as any).$config = App.config;
+
+        const MessagesView = Vue.extend(messagesView);
+        const MessageInputView = Vue.extend(messageInputView);
+        const RoomsView = Vue.extend(roomsView);
+
+        const eventBus = new Vue();
+        // Add chat channel
+        App.chat = createChatChannel(App.cable, eventBus);
+        eventBus.$on('subscribeRoom', (room) => {
+            const screenNames = [room.screen_name];
+            ( App.chat as any ).subscribe(this.accessToken, screenNames);
+        });
+        // Play sound when received a message
+        eventBus.$on('messageReceived', (room, message) => {
+            App.config.isSoundEnabled().then((enabled: boolean) => {
+                if(enabled) {
+                    const ring = document.body.querySelector('audio.ring') as HTMLAudioElement;
+                    ring.play();
+                }
+            });
+        });
+        // store latest choosen room
+        eventBus.$on('changeRoom', room => App.config.setActiveRoom(room));
+
+        new RoomsView({
+            el: '#chatapp .sidebar',
+            propsData: {
+                eventBus,
+            },
+        });
+        new MessagesView({
+            el: '#chatapp .talks',
+            propsData: {
+                eventBus,
+            },
+        });
+        new MessageInputView({
+            el: '#say_text',
+            propsData: {
+                eventBus,
+            },
+        });
+    }
+}
+
 document.addEventListener('DOMContentLoaded', () => {
     // get access token via DOM
     const accessToken = document.head.querySelector('meta[name="access-token"]').getAttribute('content');
-    // automatically insert some headers for all requests
-    (Vue.prototype as any).$http = axios.create({
-        xsrfHeaderName: 'X-CSRF-Token',
-        withCredentials: true,
-        baseURL: '/api',
-        headers: {
-            'X-Access-Token': accessToken,
-        },
-    });
-    // can access global configuration
-    (Vue.prototype as any).$config = App.config;
-
-    const MessagesView = Vue.extend(messagesView);
-    const MessageInputView = Vue.extend(messageInputView);
-    const RoomsView = Vue.extend(roomsView);
-
-    const eventBus = new Vue();
-    // Add chat channel
-    App.chat = createChatChannel(App.cable, eventBus);
-    eventBus.$on('subscribeRoom', (room) => {
-        const screenNames = [room.screen_name];
-        const accessToken = document.head.querySelector('meta[name="access-token"]').getAttribute('content');
-        ( App.chat as any ).subscribe(accessToken, screenNames);
-    });
-    // Play sound when received a message
-    eventBus.$on('messageReceived', (room, message) => {
-        App.config.isSoundEnabled().then((enabled: boolean) => {
-            if(enabled) {
-                const ring = document.body.querySelector('audio.ring') as HTMLAudioElement;
-                ring.play();
-            }
-        });
-    });
-    // store latest choosen room
-    eventBus.$on('changeRoom', room => App.config.setActiveRoom(room));
-
-    new RoomsView({
-        el: '#chatapp .sidebar',
-        propsData: {
-            eventBus,
-        },
-    });
-    new MessagesView({
-        el: '#chatapp .talks',
-        propsData: {
-            eventBus,
-        },
-    });
-    new MessageInputView({
-        el: '#say_text',
-        propsData: {
-            eventBus,
-        },
-    });
+    new Chat(accessToken).start();
 });
