2dffe5fe0817e184ed22ea359d929bdb87d71b69
supermomonga <hi@supermomonga.com>
2017-05-02 08:28:22 +0900

    fix regexp

--- app/message_filters/image_tag_message_filter.rb
+++ app/message_filters/image_tag_message_filter.rb
@@ -1,5 +1,5 @@
 class ImageTagMessageFilter < ApplicationMessageFilter
-  IMAGE_URL_REGEX = %r`(https?://[^s]+\.(?:jpg|JPG|jpeg|JPEG|png|PNG|gif|GIF)(?:#[^s]*|\?[^s]*)?)(?:$|\s)`
+  IMAGE_URL_REGEX = %r`(https?://[^\s]+\.(?:jpg|JPG|jpeg|JPEG|png|PNG|gif|GIF)(?:#[^\s]*|\?[^\s]*)?)(?:$|\s)`
   def self.filter(text)
     text.gsub(IMAGE_URL_REGEX) { |s|
       m = s.match(IMAGE_URL_REGEX)
