7ee60bcc219bf57e51a0526f8923a5b6f758b6e0
supermomonga <hi@supermomonga.com>
2017-05-02 20:39:30 +0900

    SAIAKU workaround to determine user by passing access_token via Sec-WebSocket-Protocol header

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,7 +1,23 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+    identified_by :current_user
 
     def connect
+      self.current_user = find_verified_user
+    end
+
+    protected
+    def find_verified_user
+      sec_websocket_protocol = request.headers[:HTTP_SEC_WEBSOCKET_PROTOCOL]
+      access_token = sec_websocket_protocol.split(/,\s*/).find { |t|
+        t.start_with?('access-token-')
+      }&.sub(/^access-token-/, '')
+      user = AccessToken.find_by(token: access_token)&.user
+      if user
+        user
+      else
+        reject_unauthoried_connection
+      end
     end
 
   end
--- assets/javascripts/chat.ts
+++ assets/javascripts/chat.ts
@@ -12,6 +12,13 @@ const roomsView = require('./view/rooms.vue');
 const roomMenuView = require('./view/room-menu.vue');
 
 // Initialize global context
+
+// SAIAKU Workaround
+const WebSocketOrigin = ( window as any ).WebSocket;
+( window as any ).WebSocket = (url, protocols) => {
+    const accessToken = document.head.querySelector('meta[name="access-token"]').getAttribute('content');
+    return new WebSocketOrigin(url, protocols.concat(`access-token-${accessToken}`))
+};
 const App = {
     cable: ActionCable.createConsumer(),
     chat: {},
