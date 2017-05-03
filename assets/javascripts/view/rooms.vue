5a5c3860a028a236b5d4508fbb30032a87fcf7e3
supermomonga <hi@supermomonga.com>
2017-05-03 12:04:51 +0900

    room member mock html

--- assets/javascripts/view/rooms.vue
+++ assets/javascripts/view/rooms.vue
@@ -5,7 +5,14 @@
                 <a v-bind:href="roomUrl(room)"><span v-if="room.unread_count > 0" class="badge pull-right">{{room.unread_count}}</span>{{room.screen_name}}<i class="fa fa-lock" v-if="room.private" /></a>
             </li>
         </ul>
+        <div class="profiles">
+          <a href="/@user"><img src="/assets/default_avatar_18-061e9a0d129a0480cdcf0dd1bbafbabc7fbec8aa9130b6b373c08b4f524b666a.png" /></a>
+          <a href="/@user"><img src="/assets/default_avatar_18-061e9a0d129a0480cdcf0dd1bbafbabc7fbec8aa9130b6b373c08b4f524b666a.png" /></a>
+          <a href="/@user"><img src="/assets/default_avatar_18-061e9a0d129a0480cdcf0dd1bbafbabc7fbec8aa9130b6b373c08b4f524b666a.png" /></a>
+          <a href="/@user"><img src="/assets/default_avatar_18-061e9a0d129a0480cdcf0dd1bbafbabc7fbec8aa9130b6b373c08b4f524b666a.png" /></a>
+        </div>
     </div>
 </template>
 
 <script src="./rooms.js"></script>
+<style src="./rooms.css"></style>
