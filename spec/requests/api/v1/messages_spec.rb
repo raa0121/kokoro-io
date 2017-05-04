92d698f99d981cede9716aa8e69ff3f67d9ff96e
mtwtkman <punipuniomochi@gmail.com>
2017-05-04 20:00:52 +0900

    added a messages_spec

--- spec/requests/api/v1/messages_spec.rb
+++ spec/requests/api/v1/messages_spec.rb
@@ -1,3 +1,4 @@
+require 'time'
 require 'rails_helper'
 
 RSpec.describe API::Root::V1::Messages, type: :request do
@@ -19,40 +20,47 @@ RSpec.describe API::Root::V1::Messages, type: :request do
 
   let(:headers) { {'X-Access-Token' => user.primary_access_token.token} }
   let(:path) { "/api/v1/rooms/#{ room.screen_name }/messages" }
+  let(:params) { {} }
 
   describe 'GET /api/v1/rooms/:screen_name/messages' do
+    let (:request) do
+      get path, headers: headers, params: @params
+    end
+
     it 'returns 200' do
-      get path, headers: headers, params: {}
+      request
       expect(response.status).to eq(200)
     end
 
     it 'returns messages default length' do
-      get path, headers: headers, params: {}
+      request
       expect(json(response.body).length).to eq(30)
     end
 
     it 'returns messages with limit' do
-      get path, headers: headers, params: {
-        limit: 10
-      }
+      @params = { limit: 10 }
+      request
       expect(json(response.body).length).to eq(10)
     end
 
     it 'returns messages with before_id' do
-      get path, headers: headers, params: {
-        before_id: latest_message.id - 20
-      }
+      @params = { before_id: latest_message.id - 20 }
+      request
       expect(json(response.body).length).to eq(20)
     end
 
     it 'returns messages with limit and before_id' do
-      get path, headers: headers, params: {
-        limit: 10,
-        before_id: latest_message.id - 5
-      }
+      @params = { limit: 10, before_id: latest_message.id - 5 }
+      request
       expect(json(response.body).length).to eq(5)
     end
 
+    context 'messages' do
+      it 'includes iso8601 formated published_at field' do
+        @params = { limit: 1 }
+        request
+        expect(Time.iso8601(json(response.body)[0]['published_at'])).to be_truthy
+      end
+    end
   end
-
 end
