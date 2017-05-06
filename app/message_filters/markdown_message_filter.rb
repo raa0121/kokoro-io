6345d4f1b361a48c25dfc9de1a2bd15b58b2d12f
mtwtkman <punipuniomochi@gmail.com>
2017-05-06 16:33:04 +0900

    fixed #196

--- app/message_filters/markdown_message_filter.rb
+++ app/message_filters/markdown_message_filter.rb
@@ -1,9 +1,16 @@
+require 'rouge/plugins/redcarpet'
+
 class MarkdownMessageFilter < ApplicationMessageFilter
+  class HTML < Redcarpet::Render::HTML
+    include Rouge::Plugins::Redcarpet
+  end
+
   def self.filter(text)
     extensions = {
       no_infra_emphasis: true,
       autolink: false,
       disable_indented_code_blocks: true,
+      fenced_code_blocks: true,
       strikethrough: false,
       lax_spacing: true,
       underline: true,
@@ -11,7 +18,7 @@ class MarkdownMessageFilter < ApplicationMessageFilter
       quote: true,
       footnotes: false,
     }
-    renderer = Redcarpet::Render::HTML.new(
+    renderer = HTML.new(
       filter_html: false,
       escape_html: false,
       no_images: false,
