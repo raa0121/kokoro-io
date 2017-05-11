e02cf5d5f09c52966cd90e98a282d5b045b78281
supermomonga <hi@supermomonga.com>
2017-05-11 22:46:47 +0900

    fix tests

--- spec/message_filters/url_message_filter_spec.rb
+++ spec/message_filters/url_message_filter_spec.rb
@@ -8,20 +8,20 @@ RSpec.describe URLMessageFilter do
 
     it '標準URLの場合' do
       text = <<-EOS
-https://www.youtube.com/watch?v=#{id}
+<p><a href="https://www.youtube.com/watch?v=#{id}" target="_blank">https://www.youtube.com/watch?v=#{id}</a></p>
     EOS
       expected = <<-EOS
-<div><iframe width="560" height="315" src="https://www.youtube.com/embed/#{id}" frameborder="0" allowfullscreen></iframe></div>
+<p><div><iframe width="560" height="315" src="https://www.youtube.com/embed/#{id}" frameborder="0" allowfullscreen></iframe></div></p>
     EOS
 expect(URLMessageFilter.filter(text)).to eq(expected)
     end
 
     it '短縮URLの場合' do
       text = <<-EOS
-https://youtu.be/#{id}
+<p><a href="https://youtu.be/#{id}" target="_blank">https://youtu.be/#{id}</a></p>
     EOS
       expected = <<-EOS
-<div><iframe width="560" height="315" src="https://www.youtube.com/embed/#{id}" frameborder="0" allowfullscreen></iframe></div>
+<p><div><iframe width="560" height="315" src="https://www.youtube.com/embed/#{id}" frameborder="0" allowfullscreen></iframe></div></p>
     EOS
 expect(URLMessageFilter.filter(text)).to eq(expected)
     end
@@ -31,10 +31,10 @@ expect(URLMessageFilter.filter(text)).to eq(expected)
   it 'droplr タグを正しく挿入する' do
     url = 'http://d.pr/i/xxxx'
     text = <<-EOS
-#{url}
+<p><a href="#{url}" target="_blank">#{url}</a></p>
     EOS
     expected = <<-EOS
-<div><a class="img-thumbnail" href="#{url}" target="_blank"><img class="thumb" src="#{url}+" /></a></div>
+<p><div><a class="img-thumbnail" href="#{url}" target="_blank"><img class="thumb" src="#{url}+"></a></div></p>
     EOS
     expect(URLMessageFilter.filter(text)).to eq(expected)
   end
@@ -42,20 +42,20 @@ expect(URLMessageFilter.filter(text)).to eq(expected)
   it 'imgur タグを正しく挿入する' do
     id = 'xxxx'
     text = <<-EOS
-https://imgur.com/#{id}
+<p><a href="https://imgur.com/#{id}" target="_blank">https://imgur.com/#{id}</a></p>
     EOS
     expected = <<-EOS
-<div><a class="img-thumbnail" href="https://imgur.com/#{id}" target="_blank"><img class="thumb" src="https://i.imgur.com/#{id}.png" /></a></div>
+<p><div><a class="img-thumbnail" href="https://imgur.com/#{id}" target="_blank"><img class="thumb" src="https://i.imgur.com/#{id}.png"></a></div></p>
     EOS
     expect(URLMessageFilter.filter(text)).to eq(expected)
   end
 
   it 'img タグを正しく挿入する' do
     text = <<-EOS
-https://example.com/image.png
+<p><a href="https://example.com/image.png" target="_blank">https://example.com/image.png</a></p>
     EOS
     expected = <<-EOS
-<div><a class="img-thumbnail" href="https://example.com/image.png" target="_blank"><img class="thumb" src="https://example.com/image.png" /></a></div>
+<p><div><a class="img-thumbnail" href="https://example.com/image.png" target="_blank"><img class="thumb" src="https://example.com/image.png"></a></div></p>
     EOS
     expect(URLMessageFilter.filter(text)).to eq(expected)
   end
@@ -63,10 +63,10 @@ https://example.com/image.png
   it 'a タグを正しく挿入する' do
     url = 'https://example.com/'
     text = <<-EOS
-#{url}
+<p><a href="#{url}" target="_blank">#{url}</a></p>
     EOS
     expected = <<-EOS
-<a href="#{url}" target="_blank">#{url}</a>
+<p><a href="#{url}" target="_blank">#{url}</a></p>
     EOS
     expect(URLMessageFilter.filter(text)).to eq(expected)
   end
